27b12a4aa124d6eafd8dc7f4ffa0a345fea15ebd
REGISTRE JEFF <=>
2021-03-31 20:52:04 +0200

    init the structure of the map

--- src/index.js
+++ src/index.js
@@ -3,3 +3,40 @@ import ReactDOM from 'react-dom';
 
 import './index.scss';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      worldMap: [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
+      ]
+    };
+  }
+
+  world() {
+    const { worldMap } = this.state;
+    return (
+      <table>
+        {worldMap.map((line) => (
+          <tr>
+            {line.map((column) => (
+              <td>{column}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
+    );
+  }
+
+  render() {
+    return this.world();
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
